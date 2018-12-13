0d7e5bcc104b2c26cdfe270a8dde1dcb60c728c5
AlIbay8 <43456952+AlIbay8@users.noreply.github.com>
2018-12-12 18:48:56 -0800

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 setInterval(function() {
-    $("button").css('left', Math.random() * 700);
-    $("button").css('top', Math.random() * 700);
+    $(".move").css('left', Math.random() * 700);
+    $(".move").css('top', Math.random() * 700);
 }, 500);
 
-$("button").click(function() {
-    $(".riddle").text("My first is in sentience but not in instinct. My second is in axletree but not in relater. My third is in pertain but not in retina. My fourth is in annalist but not in satanist. My fifth is in contrite but not in inciter. My sixth is in repeat but not in peat. My seventh is in instantiate but not in instant.");
+$(".move").click(function() {
+    $(".riddle").show();
+});
+
+$(".submit").click(function() {
+    var phrase = $("input").val();
+    if (phrase === "4:30") {
+    $(".reveal").html("<p>Let's play a game, Truth or...?</p>");}
 });
